8341daf275388eb30237b6104a28101fc156eaad
DarthJktu <DarthJktu@gmail.com>
2019-12-16 16:56:09 +0200

    1

--- js/main.js
+++ js/main.js
@@ -46,5 +46,48 @@ $(document).ready(function () {
 
 
     /*********  END FOOTER  ************/
+
+
+    $('.instruction').click(function () {
+        $('.video-popup').addClass('open');
+    });
+
+    $('.close-btn').click(function () {
+        $(this).closest('.wrap-popup').removeClass('open');
+    });
+
+
+    $('.header__login-btn button').click(function () {
+        $('.login').addClass('open');
+    });
+
+
+
+    $(document).click(function (e) {
+
+        if($(window).width() > 992){
+            if (e.target.closest('.wrap-popup') !== undefined && e.target == e.target.closest('.wrap-popup') ) {
+                $(e.target).find('.close-btn').click();
+            }
+
+        }
+
+    });
+
+    $('.login__tab').click(function () {
+        $('.login__tab').removeClass('active');
+        $(this).addClass('active');
+        $('.login__content-i').removeClass('active');
+        $('.login__content-i').eq($(this).index()).addClass('active');
+    });
+
+
+    $('.reg-check').click(function () {
+        if($(this).parents('.reg-tap').find('.phone').val().split('_')[0].length !== 22){
+
+        }
+    });
+
+    Inputmask({"mask": "+7 (999) 999 - 99 - 99"}).mask('.phone');
 });
 
